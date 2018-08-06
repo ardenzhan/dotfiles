ea50da8d50aceb061e803f82c92fe72e0e661aab
Arden Zhan <arden@assetpanda.com>
2018-08-05 22:18:26 -0500

    added pathogen and zsh plugins

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-echo "Hi"
+# vim-pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# zsh-syntax-highlighting
+git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# zsh-autosuggestions
+git clone git@github.com:zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
