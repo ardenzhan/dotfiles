08a02ffd98be22a59f91e03d5604151974125550
Arden Zhan <arden@protonmail.ch>
2018-11-19 15:30:26 -0600

    added fzfvim to install script

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # vim-commentary
 git clone https://github.com/tpope/vim-commentary.git ~/.vim/bundle/vim-commentary
+# fzf.vim
+git clone https://github.com/junegunn/fzf.vim.git ~/.vim/bundle/fzf.vim
 
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
