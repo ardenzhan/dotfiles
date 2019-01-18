b41d0a7cda8dd2b7a8f75dd7c935fca2774f4fe1
Arden Zhan <arden@protonmail.ch>
2019-01-17 22:40:13 -0600

    use bash vars and added manual pure theme install

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+# create parent folders
+mkdir -p ~/.vim/autoload \
+         ~/.vim/bundle \
+         ~/.zsh/zfunctions
+
 # vim-pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+curl -LSso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
-# vim-commentary
-git clone https://github.com/tpope/vim-commentary.git ~/.vim/bundle/vim-commentary
-# fzf.vim
-git clone https://github.com/junegunn/fzf.vim.git ~/.vim/bundle/fzf.vim
-# vim-signify
-git clone https://github.com/mhinz/vim-signify.git ~/.vim/bundle/vim-signify
+# vim plugins
+git clone https://github.com/tpope/vim-commentary.git $HOME/.vim/bundle/vim-commentary
+git clone https://github.com/junegunn/fzf.vim.git $HOME/.vim/bundle/fzf.vim
+git clone https://github.com/mhinz/vim-signify.git $HOME/.vim/bundle/vim-signify
 
-# zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
-# zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
+# zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.zsh/zsh-autosuggestions
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -24,3 +25,9 @@ ln -sf "$DOTFILES_DIR/.vimrc" ~
 ln -sf "$DOTFILES_DIR/.zshrc" ~
 ln -sf "$DOTFILES_DIR/.tmux.conf" ~
 ln -sf "$DOTFILES_DIR/.mongorc.js" ~
+
+# pure
+PURE_PATH="$DOTFILES_DIR/repos/pure"
+git clone https://github.com/sindresorhus/pure.git $PURE_PATH
+ln -s "$PURE_PATH/pure.zsh" "$HOME/.zsh/zfunctions/prompt_pure_setup"
+ln -s "$PURE_PATH/async.zsh" "$HOME/.zsh/zfunctions/async"
