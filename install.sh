f729b562d423aeedcc02e4be4e147a763d999e09
Arden Zhan <arden@protonmail.ch>
2019-09-26 18:39:22 -0500

    Use loops to repeatedly call functions

--- install.sh
+++ install.sh
@@ -10,26 +10,53 @@ mkdir -p $VIM/autoload \
          $ZSH/zfunctions
 
 get_repo() {
-  git clone $1 $2 2> /dev/null || git -C $2 pull
+  local repo_source=$1
+  local repo_name=$(basename $repo_source .git)
+
+  case $2 in
+    vim)
+      local local_repo="$VIM/bundle/$repo_name"
+      ;;
+    zsh)
+      local local_repo="$ZSH/$repo_name"
+      ;;
+    *)
+      echo "get_repo failed"
+      exit 1
+  esac
+
+  git clone $repo_source $local_repo 2> /dev/null || git -C $local_repo pull
 }
 
 # vim-pathogen
 curl -LSso $VIM/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-# vim plugins
-get_repo https://github.com/tpope/vim-commentary.git $VIM/bundle/vim-commentary
-get_repo https://github.com/tpope/vim-fugitive.git $VIM/bundle/vim-fugitive
-get_repo https://github.com/junegunn/fzf.vim.git $VIM/bundle/fzf.vim
-get_repo https://github.com/mhinz/vim-signify.git $VIM/bundle/vim-signify
-get_repo https://github.com/romainl/vim-cool.git $VIM/bundle/vim-cool
+
+echo "VIM"
+vim_plugins_repos=(
+  https://github.com/tpope/vim-commentary.git
+  https://github.com/tpope/vim-fugitive.git
+  https://github.com/junegunn/fzf.vim.git
+  https://github.com/mhinz/vim-signify.git
+  https://github.com/romainl/vim-cool.git
+)
+for repo in ${vim_plugins_repos[@]}; do
+  get_repo $repo vim
+done
+
 # vim colorschemes
 curl -LSso $VIM/colors/photon.vim https://raw.githubusercontent.com/axvr/photon.vim/master/colors/photon.vim
 curl -LSso $VIM/colors/nord.vim https://raw.githubusercontent.com/arcticicestudio/nord-vim/master/colors/nord.vim
 
-# zsh plugins
-get_repo https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/zsh-syntax-highlighting
-get_repo https://github.com/zsh-users/zsh-autosuggestions.git $ZSH/zsh-autosuggestions
-# zsh theme
-get_repo https://github.com/sindresorhus/pure.git $ZSH/pure
+echo "ZSH"
+zsh_repos=(
+  https://github.com/zsh-users/zsh-syntax-highlighting.git
+  https://github.com/zsh-users/zsh-autosuggestions.git
+  https://github.com/sindresorhus/pure.git
+)
+for repo in ${zsh_repos[@]}; do
+  get_repo $repo zsh
+done
+
 ln -sf "$ZSH/pure/pure.zsh" "$ZSH/zfunctions/prompt_pure_setup"
 ln -sf "$ZSH/pure/async.zsh" "$ZSH/zfunctions/async"
 
